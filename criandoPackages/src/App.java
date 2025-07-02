961fb21e6f0f096928144f768b1421dc597c8ace
Julianna Fernandes <juliannabf29@gmail.com>
2025-07-01 21:43:15 -0300

    Packages

---
+++ criandoPackages/src/App.java
@@ -0,0 +1,20 @@
+import br.com.devflix.gui.*;
+import br.com.devflix.gui.botoes.*;
+import br.com.devflix.videos.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Filme filme = new Filme(); 
+        Documentario documentario = new Documentario();
+        Novela novela = new Novela();
+        Anime anime = new Anime();
+
+        Janela janela = new Janela();
+
+        MiniaturaVideo miniaturaVideo = new MiniaturaVideo();
+
+        BotaoPlay botaoPlay = new BotaoPlay();
+
+    
+    }
+}

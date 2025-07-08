6a45c40d974f8a3e0611491f91ad12cd7c2e065e
Julianna Fernandes <juliannabf29@gmail.com>
2025-07-07 21:14:15 -0300

    Interface

---
+++ Interface/src/classes/CalculadorArea.java
@@ -0,0 +1,10 @@
+package classes;
+
+public class CalculadorArea {
+
+    public double somarAreas(FiguraGeometrica quadrado, FiguraGeometrica circulo) {
+        double totalArea = quadrado.calcularArea() + circulo.calcularArea();
+        return totalArea;
+    }
+    
+}
